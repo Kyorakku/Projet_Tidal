a536f9ef5b1ecbf8997a26df707c6162587ee8ad
PhAr3N <antonin.poulard3@orange.fr>
2020-10-15 17:27:31 +0200

    SMARTY WORKS IT'S INCREDIBLE MY FRIENDS

---
+++ templates_c/be7cf3f9ca97064ac4d8bd7b1f1111aa5699d62f_0.file.index.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-10-15 15:24:28
+  from 'C:\wamp64\www\Projet_Tidal\libs\smarty\templates\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5f8869accd4673_52902466',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'be7cf3f9ca97064ac4d8bd7b1f1111aa5699d62f' => 
+    array (
+      0 => 'C:\\wamp64\\www\\Projet_Tidal\\libs\\smarty\\templates\\index.tpl',
+      1 => 1602774842,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5f8869accd4673_52902466 (Smarty_Internal_Template $_smarty_tpl) {
+?>Bonjour, <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+, Bienvenue dans Smarty !<?php }
+}
