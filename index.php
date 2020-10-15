4e68d040f9088138008aaf7a11ff19d717cda675
PEROT Clement <clement.perot@sword-group.com>
2020-10-15 15:41:30 +0200

    BDD test connexion it works

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ session_start();
 require('./libs/smarty/Smarty.class.php');
 
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=webapp", "debian-sys-maint",  "aR7RIRZbiUZw3dYk");
+    $bdd = new PDO("mysql:host=localhost;dbname=webapp", "root",  "");
     $bdd->query("SET NAMES UTF8");
+    foreach($bdd->query('SELECT * from Products') as $row) {
+        print_r($row);
+    }
+    $bdd = null;
 } catch (Exception $e) {
     echo "Problème de connexion à la base de donnée !";
     die();
 }
 
 echo("Ça marche !!!");
+
+
+    
