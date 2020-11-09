9ebe20d924bae82146084c44fa580808cd03424f
PEROT Clement <clement.perot@sword-group.com>
2020-11-09 15:32:39 +0100

    Commit to pull

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
-<?php
-session_start();
 
-require('./libs/smarty/Smarty.class.php');
 
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=webapp', 'root',  '');
-    $bdd->query('SET NAMES UTF8');
-    $rep = $bdd->query('SELECT name from Products');
-    while ($donnees = $rep->fetch()) {
-        echo $donnees['name'] . '<br />';
-    }
-    $rep->closeCursor();
-
-    $bdd = null;
-} catch (Exception $e) {
-    echo 'Problème de connexion à la base de donnée !';
-    die();
-}
-
-//echo("Ça marche !!!");
-?>
-<?php
-require_once('libs/smarty/Smarty.class.php');
-define('template_dir', 'libs/smarty/templates/');
-define('compile_dir', 'libs/smarty/templates_c/');
-define('config_dir', 'libs/smarty/configs/');
-define('cache_dir', 'libs/smarty/cache/');
-$smarty = new Smarty();
-$smarty->assign('name','Ned');
-?>
 
 <!doctype html>
 <html lang="en">
@@ -44,15 +15,42 @@ $smarty->assign('name','Ned');
     <link href="/css/reset-min.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <?php
+        session_start();
 
-    <!-- Optional JavaScript; choose one of the two! -->
+        require('./libs/smarty/Smarty.class.php');
+
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=webapp', 'root',  '');
+            $bdd->query('SET NAMES UTF8');
+            $rep = $bdd->query('SELECT username from customers');
+            while ($donnees = $rep->fetch()) {
+                echo  $donnees['username'] . '<br/>';
+            }
+            $rep->closeCursor();
+            $bdd = null;
+
+        } catch (Exception $e) {
+            echo 'Problème de connexion à la base de donnée !';
+            die();
+        }
+
+    ?>
+    <?php
+        require_once('libs/smarty/Smarty.class.php');
+        define('template_dir', 'libs/smarty/templates/');
+        define('compile_dir', 'libs/smarty/templates_c/');
+        define('config_dir', 'libs/smarty/configs/');
+        define('cache_dir', 'libs/smarty/cache/');
+        $smarty = new Smarty();
+        $smarty->assign('name','Ned');
+    ?>
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <?php $smarty->display('libs\smarty\templates\index.tpl'); ?>
-    Bah faut mettre les articles ici quoi
+
   </body>
 </html>
 
--- login.php
+++ login.php
@@ -32,7 +32,7 @@
     <link href="./styles/css/login.css" rel="stylesheet">
   </head>
   <body class="text-center">
-    <form class="form-signin">
+    <form class="form-signin border rounded">
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
